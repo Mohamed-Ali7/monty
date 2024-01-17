63e4fa26e7a1bd15b6cf65ad65aec9c5b6a0dd27
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-17 14:18:21 +0200

    Adding (div) functionality which divides the second top element by the top element of the stack.

--- stack_2.c
+++ stack_2.c
@@ -102,3 +102,43 @@ void sub(stack_t **stack, unsigned int line_number)
 	if (*stack)
 		(*stack)->prev = NULL;
 }
+
+/**
+ * _div - Divides the second top element by the top element of the stack.
+ * @stack: Is a pointer to the pointer to the stack
+ * @line_number: Is the current line number of the opcode
+ * Return: void
+*/
+
+void _div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	int size = 0;
+
+	(void) (line_number);
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		size++;
+	}
+	if (size < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		free(args), fclose(file), free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free(args), fclose(file), free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->n /= temp->n;
+	free(temp);
+	if (*stack)
+		(*stack)->prev = NULL;
+}
