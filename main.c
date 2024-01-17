e8eba067cb3489eca1e4eb5eed4ea730dd7c61f8
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-17 14:05:35 +0200

    Adding (sub) functionaluty which subtracts the top element from the second top elemen

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	int i;
 	instruction_t inst[] = {
 		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap},
-		{"add", add}, {NULL, NULL}
+		{"add", add}, {"sub", sub}, {NULL, NULL}
 	};
 
 	if (argc != 2)
--- stack_2.c
+++ stack_2.c
@@ -66,3 +66,39 @@ void add(stack_t **stack, unsigned int line_number)
 	if (*stack)
 		(*stack)->prev = NULL;
 }
+
+/**
+ * sub - Subtracts the top element of the stack
+ * from the second top element of the stack.
+ *
+ * @stack: Is a pointer to the pointer to the stack
+ * @line_number: Is the current line number of the opcode
+ * Return: void
+*/
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	int size = 0;
+
+	(void) (line_number);
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		size++;
+	}
+	if (size < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		free(args), fclose(file), free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->n -= temp->n;
+	free(temp);
+	if (*stack)
+		(*stack)->prev = NULL;
+}
